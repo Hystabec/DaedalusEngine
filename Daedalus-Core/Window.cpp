e87989bbd59b86de9ef7ed42583741d1a4ec8cca
Hystabec <edward14577777@gmail.com>
2024-11-10 23:11:17 +0000

    added input into window
    make input separate class - might be pointless

--- Daedalus-Core/Window.cpp
+++ Daedalus-Core/Window.cpp
@@ -2,7 +2,7 @@
 
 namespace daedalusCore { namespace graphics {
 
-	void windowResize(GLFWwindow* window, int width, int height);
+	void window_Resize(GLFWwindow* window, int width, int height);
 
 	Window::Window(const char* title, int width, int height) : m_title(title), m_width(width), m_height(height)
 	{
@@ -19,7 +19,12 @@ namespace daedalusCore { namespace graphics {
 		}
 
 		glfwMakeContextCurrent(m_window);
-		glfwSetWindowSizeCallback(m_window, windowResize);
+		glfwSetWindowUserPointer(m_window, this);
+		glfwSetWindowSizeCallback(m_window, window_Resize);
+
+		glfwSetKeyCallback(m_window, key_callback);
+		glfwSetMouseButtonCallback(m_window, mouse_button_callback);
+		glfwSetCursorPosCallback(m_window, cursor_position_callback);
 
 		if (glewInit() != GLEW_OK)
 		{
@@ -28,6 +33,12 @@ namespace daedalusCore { namespace graphics {
 		}
 
 		std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
+
+		for (int i = 0; i < MAX_KEYS; i++)
+			m_keys[i] = false;
+
+		for (int i = 0; i < MAX_BUTTONS; i++)
+			m_MouseButtons[i] = false;
 	}
 
 	Window::~Window()
@@ -52,9 +63,59 @@ namespace daedalusCore { namespace graphics {
 		return glfwWindowShouldClose(m_window);
 	}
 
-	void windowResize(GLFWwindow* window, int width, int height)
+	bool Window::getKey(unsigned int keycode) const
+	{
+		if (keycode >= MAX_KEYS)
+		{
+			std::cout << "Error - keycode out of range" << std::endl;
+			return false;
+		}
+
+		return m_keys[keycode];
+	}
+
+	bool Window::getMouse(unsigned int buttonCode) const
+	{
+		if (buttonCode >= MAX_BUTTONS)
+		{
+			std::cout << "Error - buttonCode out of range" << std::endl;
+			return false;
+		}
+
+		return m_MouseButtons[buttonCode];
+	}
+
+	void Window::getMousePosition(double& xPos, double& yPos) const
+	{
+		xPos = m_mouseX;
+		yPos = m_mouseY;
+	}
+
+	void window_Resize(GLFWwindow* window, int width, int height)
 	{
 		glViewport(0, 0, width, height);
+	}
+
+	void Window::key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+	{
+		Window* t_window = (Window*)glfwGetWindowUserPointer(window);
+		
+		t_window->m_keys[key] = action != GLFW_RELEASE;
+	}
+
+	void Window::mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+	{
+		Window* t_window = (Window*)glfwGetWindowUserPointer(window);
+
+		t_window->m_MouseButtons[button] = action != GLFW_RELEASE;
+	}
+
+	void Window::cursor_position_callback(GLFWwindow* window, double xpos, double ypos)
+	{
+		Window* t_window = (Window*)glfwGetWindowUserPointer(window);
+
+		t_window->m_mouseX = xpos;
+		t_window->m_mouseY = ypos;
 	}
 
 } }
--- Daedalus-Core/Window.h
+++ Daedalus-Core/Window.h
@@ -6,6 +6,9 @@
 
 namespace daedalusCore { namespace graphics {
 
+#define MAX_KEYS	1024
+#define MAX_BUTTONS 32
+
 	class Window
 	{
 	private:
@@ -14,6 +17,9 @@ namespace daedalusCore { namespace graphics {
 		GLFWwindow* m_window;
 		bool m_closed = false;
 
+		bool m_keys[MAX_KEYS];
+		bool m_MouseButtons[MAX_BUTTONS];
+		double m_mouseX, m_mouseY;
 	public:
 		Window(const char* title, int width, int height);
 		~Window();
@@ -23,6 +29,14 @@ namespace daedalusCore { namespace graphics {
 
 		//inline int getWidth() const { return m_width; };
 		//inline int getHeight() const { return m_height; };
+
+		bool getKey(unsigned int keycode) const;
+		bool getMouse(unsigned int buttonCode) const;
+		void getMousePosition(double& xPos, double& yPos) const;
+	private:
+		static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+		static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+		static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
 	};
 
 } }
