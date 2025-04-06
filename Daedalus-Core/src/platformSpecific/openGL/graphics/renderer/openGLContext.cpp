ad7201ff15923b71d591b9cc44a066d5d23faa08
Hystabec <edward14577777@gmail.com>
2025-04-06 17:16:40 +0100

    added assert checking for OpenGL version

--- Daedalus-Core/src/platformSpecific/openGL/graphics/renderer/openGLContext.cpp
+++ Daedalus-Core/src/platformSpecific/openGL/graphics/renderer/openGLContext.cpp
@@ -20,6 +20,16 @@ namespace daedalusCore { namespace graphics {
 			if (glewInit() != GLEW_OK)
 				DD_CORE_ASSERT(false, "GLEW failed to initialize");
 		}
+
+#ifdef DD_USING_ASSERTS
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		DD_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "OpenGL version 4.5 or greater required");
+#endif // DD_USING_ASSERTS
+
 	}
 
 	void OpenGlContext::swapBuffers()
