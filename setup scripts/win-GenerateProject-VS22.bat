03a8d07833c338cdc6c0e6d3ea4437b76a2e555a
Hystabec <edward14577777@gmail.com>
2025-07-24 20:27:47 +0100

    added echo to run setup.bat to generate project files

--- setup scripts/win-GenerateProject-VS22.bat
+++ setup scripts/win-GenerateProject-VS22.bat
@@ -1,5 +1,7 @@
 @echo off
 pushd %~dp0\..\
+echo Make sure to run 'setup.bat' to install necessary vulkan libraries (If not already done)
+echo Running premake...
 call Dependencies\premake\premake5.exe vs2022
 popd
 pause
