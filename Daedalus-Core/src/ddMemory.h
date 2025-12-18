7bbd875eb372fb532d44092ae70f48a2abbb5887
Hystabec <edward14577777@gmail.com>
2025-12-18 03:31:52 +0000

    started work on intrusive pointers

--- Daedalus-Core/src/ddMemory.h
+++ Daedalus-Core/src/ddMemory.h
@@ -1,9 +1,115 @@
 #pragma once
 
+#include <atomic>
+#include <type_traits>
+
 // TO DO: Move buffer into this file as it is similarly a memory holder class
 // add should probably also being included inside core
 
 namespace daedalus {
+#pragma region IntrusivePtr
+	namespace counters {
+		namespace internals { template<class> struct IntrusiveCounter; }
+
+		struct ReferenceCounter
+		{
+			template<class>friend struct internals::IntrusiveCounter;
+
+		protected:
+			ReferenceCounter() = default;
+		private:
+			uint64_t count{ 0 };
+		};
+
+		struct AtomicReferenceCounter
+		{
+			template<class> friend struct internals::IntrusiveCounter;
+
+		protected:
+			AtomicReferenceCounter() = default;
+		private:
+			std::atomic<uint64_t> count{ 0 };
+		};
+
+		namespace internals {
+			template<class baseT>
+			struct IntrusiveCounter
+			{
+				static void increment(counters::ReferenceCounter* ptr) { ptr->count++; }
+				static void decrement(counters::ReferenceCounter* ptr)
+				{
+					ptr->count--;
+					if (!count(ptr))
+						delete static_cast<const baseT*>(ptr);
+				}
+				static uint64_t count(counters::ReferenceCounter* ptr) { return ptr->count; }
+
+				static void increment(counters::AtomicReferenceCounter* ptr) { ptr->count.fetch_add(1, std::memory_order_relaxed); }
+				static void decrement(counters::AtomicReferenceCounter* ptr)
+				{
+					ptr->count.fetch_sub(1, std::memory_order_acq_rel);
+					if (!count(ptr))
+						delete static_cast<const baseT*>(ptr);
+				}
+				static uint64_t count(counters::AtomicReferenceCounter* ptr) { return ptr->count.load(std::memory_order_relaxed); }
+			};
+		}
+
+	}
+
+	template<class T>
+	class IntrusivePtr
+	{
+		static_assert(std::is_base_of_v<counters::ReferenceCounter, T> || std::is_base_of_v<counters::AtomicReferenceCounter,T>,
+			"IntrusivePtr::T must derive from either counters::ReferenceCounter or counters::AtomicReferenceCounter");
+
+	public:
+		IntrusivePtr(T* ptr = nullptr)
+			: m_ptr(ptr)
+		{
+			if (m_ptr)
+				counters::internals::IntrusiveCounter<T>::increment(this->get());
+		};
+
+		IntrusivePtr(const IntrusivePtr& other)
+			: m_ptr(other.m_ptr)
+		{
+			if (m_ptr)
+				counters::internals::IntrusiveCounter<T>::increment(this->get());
+		}
+
+		~IntrusivePtr()
+		{
+			if (m_ptr)
+				counters::internals::IntrusiveCounter<T>::decrement(this->get());
+		}
+
+		T* get() { return m_ptr; }
+
+		void swap(IntrusivePtr& other)
+		{
+			T* temp = other.m_ptr;
+			other.m_ptr = m_ptr;
+			m_ptr = temp;
+		}
+
+		IntrusivePtr& operator =(const IntrusivePtr& other)
+		{
+			IntrusivePtr(other).swap(*this);
+			return *this;
+		}
+
+		IntrusivePtr& operator =(IntrusivePtr&& other)
+		{
+			IntrusivePtr(std::move(other)).swap(*this);
+			return *this;
+		}
+
+	private:
+		T* m_ptr;
+	};
+
+#pragma endregion
 #pragma region ScopedPtr
 
 	template<class T>
@@ -81,15 +187,6 @@ namespace daedalus {
 	{
 		return ptr.get() != nullptr;
 	}
-
-#pragma endregion
-#pragma region IntrusivePtr
-
-	template<class T>
-	class IntrusivePtr
-	{
-
-	};
 
 #pragma endregion
 }
